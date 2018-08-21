a7fee7f687df8349d942594a2a38b36a910fccb6
Eric <eric.f.meyer@gmail.com>
2018-08-20 19:57:28 -0500

    BinBuilder - more functions and tests

--- casino/roulette/binbuilder.py
+++ casino/roulette/binbuilder.py
@@ -10,18 +10,31 @@ from casino.roulette.game import RouletteGame as RoG
 
 class BinBuilder(object):
     def __init__(self):
+        """Create an instance of BinBuilder."""
         pass
 
     def build_bins(self, wheel):
-        self.build_five_bets(wheel)
-        self.build_straight_bets(wheel)
+        """Build the bins of the wheel.
+
+        :param wheel: the wheel with bins to populate with outcomes.
+        """
+        self.add_five_bets(wheel)
+        self.add_straight_bets(wheel)
+        self.add_split_bets(wheel)
+        self.add_street_bets(wheel)
+        self.add_corner_bets(wheel)
+        self.add_line_bets(wheel)
+        self.add_column_bets(wheel)
+        self.add_dozen_bets(wheel)
 
     @classmethod
-    def build_five_bets(cls, wheel):
-        wheel.add_outcome(0, Outcome(RoG.five_bet_name, RoG.five_bet_odds))
+    def add_five_bets(cls, wheel):
+        """Add five bet outcomes to the bin 0 and 00"""
+        for i in (0, 37):
+            wheel.add_outcome(i, Outcome(RoG.five_bet_name, RoG.five_bet_odds))
 
     @classmethod
-    def build_straight_bets(cls, wheel):
+    def add_straight_bets(cls, wheel):
         for n in range(0, 37):
             wheel.add_outcome(n, Outcome(
                 '{} {}'.format(RoG.straight_bet_name, n),
@@ -32,20 +45,40 @@ class BinBuilder(object):
             '{} {}'.format(RoG.straight_bet_name, '00'),
             RoG.straight_bet_odds))
 
-    def build_split_bets(self, wheel):
+    @classmethod
+    def add_split_bets(cls, wheel):
+        # add all {n, n + 1} outcomes
+        for row in range(0, 12):
+            cls.helper_add_split_bets(wheel, row * 3 + 1, 1)
+            cls.helper_add_split_bets(wheel, row * 3 + 2, 1)
+
+        # add all {n, n + 3} outcomes
+        for i in range(1, 34):
+            cls.helper_add_split_bets(wheel, i, 3)
+
+    @classmethod
+    def helper_add_split_bets(cls, wheel, n_bin, inc):
+        oc = Outcome(
+            '{} {}-{}'.format(RoG.split_bet_name, n_bin, n_bin + inc),
+            RoG.split_bet_odds)
+        wheel.add_outcome(n_bin, oc)
+        wheel.add_outcome(n_bin + inc, oc)
+
+    @classmethod
+    def add_street_bets(cls, wheel):
         pass
 
-    def build_street_bets(self, wheel):
+    @classmethod
+    def add_corner_bets(cls, wheel):
         pass
 
-    def build_corner_bets(self, wheel):
+    @classmethod
+    def add_line_bets(cls, wheel):
         pass
 
-    def build_line_bets(self, wheel):
+    @classmethod
+    def add_dozen_bets(cls, wheel):
         pass
 
-    def build_dozen_bets(self, wheel):
-        pass
-
-    def build_column_bets(self, wheel):
+    def add_column_bets(self, wheel):
         pass
--- tests/test_binbuilder.py
+++ tests/test_binbuilder.py
@@ -2,43 +2,103 @@ import unittest
 
 from casino.roulette.binbuilder import BinBuilder
 from casino.roulette.outcome import Outcome
-from casino.roulette.game import RouletteGame as RoG
 from casino.roulette.wheel import Wheel
 
 
 class BinBuilderTest(unittest.TestCase):
     """Test the implementation of the class BinBuilder."""
 
+    straight_bet_odds = 35
+    split_bet_odds = 17
+    street_bet_odds = 11
+    corner_bet_odds = 8
+    five_bet_odds = 6
+    line_bet_odds = 5
+    dozen_bet_odds = 2
+    column_bet_odds = 2
+    even_money_bet_odds = 1
+
+    straight_bet_name = 'Number'
+    split_bet_name = 'Split'
+    street_bet_name = 'Street'
+    corner_bet_name = 'Corner'
+    five_bet_name = 'Five 00-0-1-2-3'
+    line_bet_name = 'Line'
+    dozen_bet_name = 'Dozen'
+    column_bet_name = 'Column'
+    black_bet_name = 'Black'
+    red_bet_name = 'Red'
+    even_bet_name = 'Even'
+    odd_bet_name = 'Odd'
+    high_bet_name = 'High'
+    low_bet_name = 'Low'
+
     def setUp(self):
-        self.bin_builder = BinBuilder()
         self.wheel = Wheel()
+        BinBuilder().build_bins(self.wheel)
 
     def test_can_create_bin_builder(self):
-        self.assertIsNotNone(self.bin_builder)
+        self.assertIsNotNone(BinBuilder())
 
-    def test_builder_generates_five_bets(self):
-        self.bin_builder.build_five_bets(self.wheel)
+    # five bets
+    def test_five_bets_for_zero(self):
         actual_bin = self.wheel.get_bin(0)
-        expected = Outcome(RoG.five_bet_name, RoG.five_bet_odds)
+        expected = Outcome(self.five_bet_name, self.five_bet_odds)
         self.assertTrue(expected in actual_bin)
 
-    def test_builder_generates_straight_bets_for_all_except_double_zero(self):
-        self.bin_builder.build_straight_bets(self.wheel)
+    def test_five_bets_for_dbl_zero(self):
+        actual_bin = self.wheel.get_bin(37)
+        expected = Outcome(self.five_bet_name, self.five_bet_odds)
+        self.assertTrue(expected in actual_bin)
+
+    def test_no_other_five_bets(self):
+        five_bet = Outcome(self.five_bet_name, self.five_bet_odds)
+        for i in range(1, 37):
+            self.assertTrue(five_bet not in self.wheel.get_bin(i))
+
+    # straight bets
+    def test_straight_bets_for_all_except_double_zero(self):
         for bin_number in range(0, 37):
             actual_bin = self.wheel.get_bin(bin_number)
             expected = Outcome(
-                '{} {}'.format(RoG.straight_bet_name, bin_number),
-                RoG.five_bet_odds)
+                '{} {}'.format(self.straight_bet_name, bin_number),
+                self.straight_bet_odds)
             self.assertTrue(expected in actual_bin)
 
-    def test_builder_generates_straight_bets_for_double_zero(self):
-        self.bin_builder.build_straight_bets(self.wheel)
+    def test_straight_bets_for_dbl_zero(self):
         bin_number = 37
         actual_bin = self.wheel.get_bin(bin_number)
         expected = Outcome(
-            '{} {}'.format(RoG.straight_bet_name, '00'),
-            RoG.five_bet_odds)
+            '{} {}'.format(self.straight_bet_name, '00'),
+            self.straight_bet_odds)
         self.assertTrue(expected in actual_bin)
+
+    # split bets
+    def test_all_split_bets(self):
+        for row in range(0, 12):
+            self.helper_split_bets_test(row * 3 + 1, 1)
+            self.helper_split_bets_test(row * 3 + 2, 1)
+
+        for i in range(1, 34):
+            self.helper_split_bets_test(i, 3)
+
+    def helper_split_bets_test(self, n, inc):
+        actual_bin1 = self.wheel.get_bin(n)
+        actual_bin2 = self.wheel.get_bin(n + inc)
+        exp_1 = Outcome('{} {}-{}'.format(self.split_bet_name, n, n + inc),
+                        self.split_bet_odds)
+        self.assertTrue(exp_1 in actual_bin1)
+        self.assertTrue(exp_1 in actual_bin2)
+
+    def test_no_split_bets_for_zeros(self):
+        zero = self.wheel.get_bin(0)
+        dbl_zero = self.wheel.get_bin(37)
+        for oc in zero | dbl_zero:
+            self.assertTrue(self.split_bet_name not in str(oc))
+
+    # street bets
+    def test_street_bets(self):
+        pass
 
 
 if __name__ == '__main__':
