9a1249fb21a3b8c38ad0392e6d398aac8335609b
Eric <eric.f.meyer@gmail.com>
2018-08-21 19:44:44 -0500

    Finish Bet class

--- casino/roulette/bet.py
+++ casino/roulette/bet.py
@@ -15,7 +15,39 @@ class Bet(object):
     """
 
     def __init__(self, amount, outcome):
+        """Create a new bet of a specific amount on a specific outcome.
+
+        :param amount: the amount bet on the outcome.
+        :param outcome: the outcome being bet on.
+        """
+        # TODO: associate bet with a player
         self.amount = amount
         self.outcome = outcome
 
-    pass
+    def win_amount(self):
+        """Return the amount won.
+
+        :return: (int) amount won.
+        """
+        return self.amount + self.outcome.win_amount(self.amount)
+
+    def lose_amount(self):
+        """Return the amount lost.
+
+        :return: (int) amount lost.
+        """
+        return self.amount
+
+    def __str__(self):
+        """String representation of the form 'amount on outcome'.
+
+        :return: (str) 'amount on outcome'.
+        """
+        return '{:d} on {:s}'.format(self.amount, str(self.outcome))
+
+    def __repr__(self):
+        """String representation of the form 'Bet(amount, outcome)'.
+
+        :return: (str) 'Bet(amount, outcome)'.
+        """
+        return 'Bet({:d}, {:s})'.format(self.amount, str(self.outcome))
--- tests/test_bet.py
+++ tests/test_bet.py
@@ -1,16 +1,42 @@
 import unittest
 
 from casino.roulette.bet import Bet
+from casino.roulette.game import RouletteGame as RoG
+from casino.roulette.outcome import Outcome
 
 
 class BetTest(unittest.TestCase):
     """Test the implementation of the class Bet."""
 
     def setUp(self):
-        self.bet = Bet()
+        self.black_bet = Bet(10, Outcome(RoG.black_bet_name,
+                                         RoG.even_money_bet_odds))
+        self.straight_bet = Bet(5, Outcome(
+            RoG.straight_bet_name + ' 35', RoG.straight_bet_odds)
+        )
 
     def test_can_create_bet(self):
-        self.assertIsNotNone(self.bet)
+        self.assertIsNotNone(self.black_bet)
+
+    def test_win_amount_is_correct_for_black_bet(self):
+        expected = 20   # 10 * 1 + 10
+        self.assertEqual(expected, self.black_bet.win_amount())
+
+    def test_win_amount_is_correct_for_straight_bet(self):
+        expected = 180  # 5 * 35 + 5
+        self.assertEqual(expected, self.straight_bet.win_amount())
+
+    def test_lose_amount_is_correct(self):
+        self.assertEqual(10, self.black_bet.lose_amount())
+        self.assertEqual(5, self.straight_bet.lose_amount())
+
+    def test_string_representation(self):
+        self.assertEqual("10 on Black (1:1)", str(self.black_bet))
+        self.assertEqual("5 on Number 35 (35:1)", str(self.straight_bet))
+
+    def test_repr(self):
+        self.assertEqual("Bet(10, Black (1:1))", repr(self.black_bet))
+        self.assertEqual("Bet(5, Number 35 (35:1))", repr(self.straight_bet))
 
 
 if __name__ == '__main__':
