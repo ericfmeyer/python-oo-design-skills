684a520929824fa93cb9980a7e46740f28c2b6ea
Eric Meyer <MeyerEricF@JohnDeere.com>
2018-08-22 16:24:39 -0500

    Table class - WIP (need to finish is_valid())

--- casino/roulette/table.py
+++ casino/roulette/table.py
@@ -5,8 +5,22 @@ Implements the Table class.
 """
 
 
+class InvalidBet(Exception):
+    """InvalidBet is raised when the player attempts to place a bet which
+    exceeds the table's limit.
+    """
+    pass
+
+
 class Table(object):
     """Collection of bets placed by players on outcomes.
+
+    A table has a betting limit, and the sum of all players' bets
+    must be less than or equal to this limit.
+
+    A table also has a betting minimum.
+
+    We assume a single player in the simulation.
 
     Collaborators:
         - Game: uses table to compute the amount won and lost.
@@ -15,4 +29,47 @@ class Table(object):
         - Player: uses table to place bets on specific outcomes.
     """
 
-    pass
+    TABLE_MIN = 10
+    TABLE_LIMIT = 300
+
+    def __init__(self):
+        self.limit = self.TABLE_LIMIT
+        self.minimum = self.TABLE_MIN
+        self.bets = list()
+
+    def place_bet(self, bet):
+        """Add the bet to the list of working bets.
+
+        :param bet: a bet instance to add to the table.
+        """
+        self.bets.append(bet)
+
+    def __iter__(self):
+        """Return an iterator over the available list of bets."""
+        return self.bets[:].__iter__()
+
+    def __str__(self):
+        """Return string of all the current bets.
+
+        :return: (str) string of all the bets.
+        """
+        return '\n'.join(str(bet) for bet in self.bets)
+
+    def __repr__(self):
+        """Return string of the form Table( bet, bet, bet ).
+
+        :return: (str) Table( bet, bet, bet ).
+        """
+        return 'Table( {} )'.format(', '.join(repr(bet) for bet in self.bets))
+
+    def is_valid(self):
+        total = 0
+        for bet in self.bets:
+            if bet.amount < self.TABLE_MIN:
+                raise InvalidBet
+            total += bet.amount
+
+        if total > self.TABLE_LIMIT:
+            raise InvalidBet
+
+        return True
--- tests/test_table.py
+++ tests/test_table.py
@@ -1,6 +1,9 @@
 import unittest
 
+from casino.roulette.bet import Bet
+from casino.roulette.outcome import Outcome
 from casino.roulette.table import Table
+from casino.roulette.table import InvalidBet
 
 
 class TableTest(unittest.TestCase):
@@ -8,9 +11,70 @@ class TableTest(unittest.TestCase):
 
     def setUp(self):
         self.table = Table()
+        self.bet_split = Bet(10, Outcome("Split 1-2", 17))
+        self.bet_straight = Bet(5, Outcome("Number 17", 35))
+        self.bet_red = Bet(25, Outcome("Red", 1))
 
     def test_can_create_table(self):
         self.assertIsNotNone(self.table)
+
+    def test_place_bet_adds_bet_to_table(self):
+        self.table.place_bet(self.bet_split)
+        self.assertEqual(1, len(self.table.bets))
+
+    def test_place_bet_can_add_same_bet(self):
+        self.table.place_bet(self.bet_split)
+        self.table.place_bet(self.bet_split)
+        self.assertEqual(2, len(self.table.bets))
+        for elem in self.table:
+            self.assertEqual(self.bet_split, elem)
+
+    def test_place_bet_can_different_bets(self):
+        self.table.place_bet(self.bet_split)
+        self.table.place_bet(self.bet_red)
+        self.table.place_bet(self.bet_straight)
+        self.assertEqual(self.bet_split, self.table.bets[0])
+        self.assertEqual(self.bet_red, self.table.bets[1])
+        self.assertEqual(self.bet_straight, self.table.bets[2])
+
+    def test_iter_returns_iterator_on_list_of_bets(self):
+        self.table.place_bet(self.bet_split)
+        for bet in self.table:
+            self.assertIsInstance(bet, Bet)
+
+    def test_string_representation_for_one_bet(self):
+        self.table.place_bet(self.bet_split)
+        expected = "10 on Split 1-2 (17:1)"
+        self.assertEqual(expected, str(self.table))
+
+    def test_string_representation_for_many_bets(self):
+        self.table.place_bet(self.bet_split)
+        self.table.place_bet(self.bet_red)
+        expected = "10 on Split 1-2 (17:1)\n25 on Red (1:1)"
+        self.assertEqual(expected, str(self.table))
+
+    def test_representation_for_one_bet(self):
+        self.table.place_bet(self.bet_split)
+        expected = "Table( Bet(10, Split 1-2 (17:1)) )"
+        self.assertEqual(expected, repr(self.table))
+
+    def test_representation_for_many_bets(self):
+        self.table.place_bet(self.bet_split)
+        self.table.place_bet(self.bet_red)
+        expected = "Table( Bet(10, Split 1-2 (17:1)), Bet(25, Red (1:1)) )"
+        self.assertEqual(expected, repr(self.table))
+
+
+# TODO: finish test of is_valid()
+# class TableIsValidTest(unittest.TestCase):
+#     def setUp(self):
+#         self.table = Table()
+#         self.under_min_bet = Bet(5, Outcome("Red", 1))
+#         self.one_bet_over_max = Bet(301, Outcome("Red", 1))
+#
+#     def test_is_valid_raises_exception_for_bet_under_minimum(self):
+#         self.table.place_bet(self.under_min_bet)
+#         self.assertRaises(InvalidBet, self.table.is_valid())
 
 
 if __name__ == '__main__':
